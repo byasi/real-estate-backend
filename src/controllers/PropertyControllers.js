4e57a5de55836739dc3dcc27564063c592af38c7
byasi <solomonbyasi@gmail.com>
2023-05-16 19:36:59 +0300

    fixed bugs

--- src/controllers/PropertyControllers.js
+++ src/controllers/PropertyControllers.js
@@ -1,135 +1,140 @@
-const PropertyServices = require('../services/PropertyServices');
-
+const PropertyServices = require("../services/PropertyServices");
 
 class PropertyControllers {
-    static async registerProperty(req,res,next){
-        try {
-            const propertyDetails = {
-                image: `http://localhost:5000/uploads/properties/${req.file.filename}`,
-                ...req.body
-            };
+  static async registerProperty(req, res, next) {
+    try {
+      const propertyDetails = {
+        image: `http://localhost:5000/properties/${req.file.filename}`,
+        ...req.body,
+      };
 
-            const propertyExists = await PropertyServices.findPropertyByName(req.body.name);
+      const propertyExists = await PropertyServices.findPropertyByName(
+        req.body.name
+      );
 
-            if (propertyExists) {
-                return res.status(409).json({
-                    status: res.statusCode,
-                    message: 'Property already exists'
-                })
-            } else {
-
-                //add the property
-                const createdProperty = await PropertyServices.addProperty({...propertyDetails, balance: req.body.price});
-                return res.status(201).json({
-                    status: res.statusCode,
-                    message: 'Property created successfully',
-                    data: createdProperty
-                })
-            }
-
-        } catch (error) {
-            return next(error);
-        }
+      if (propertyExists) {
+        return res.status(409).json({
+          status: res.statusCode,
+          message: "Property already exists",
+        });
+      } else {
+        //add the property
+        const createdProperty = await PropertyServices.addProperty({
+          ...propertyDetails,
+          balance: req.body.price,
+        });
+        return res.status(201).json({
+          status: res.statusCode,
+          message: "Property created successfully",
+          data: createdProperty,
+        });
+      }
+    } catch (error) {
+      return next(error);
     }
+  }
 
-    //find a single property
+  //find a single property
 
-    static async findSingleProperty(req,res,next){
-        try {
-            const property = await PropertyServices.getPropertyById(req.params.id);
-            if (!property){
-                return res.status(409).json({
-                    status: res.statusCode,
-                    message: 'Property not found'
-                })
-            }else{
-                return res.status(200).json({
-                    status: res.statusCode,
-                    data: property
-                })
-            }
-        } catch (error) {
-            return next(error);
-        }
+  static async findSingleProperty(req, res, next) {
+    try {
+      const property = await PropertyServices.getPropertyById(req.params.id);
+      if (!property) {
+        return res.status(409).json({
+          status: res.statusCode,
+          message: "Property not found",
+        });
+      } else {
+        return res.status(200).json({
+          status: res.statusCode,
+          data: property,
+        });
+      }
+    } catch (error) {
+      return next(error);
     }
+  }
 
-        //find all property
+  //find all property
 
-    static async getAllProperty(req,res,next){
-        try {
-            const getProperty = await PropertyServices.findAllProperty();
-            if(getProperty.length === 0){
-                return res.status(404).json({
-                    status: res.statusCode,
-                    message:'No property found'
-                })
-            }else {
-                return res.status(200).json({
-                    status: res.statusCode,
-                    data: getProperty
-                })
-            }                
-        } catch (error) {
-            return next(error);
-        }
+  static async getAllProperty(req, res, next) {
+    try {
+      const getProperty = await PropertyServices.findAllProperty();
+      if (getProperty.length === 0) {
+        return res.status(404).json({
+          status: res.statusCode,
+          message: "No property found",
+        });
+      } else {
+        return res.status(200).json({
+          status: res.statusCode,
+          data: getProperty,
+        });
+      }
+    } catch (error) {
+      return next(error);
     }
+  }
 
+  // DELETE PROPERTY
 
-    // DELETE PROPERTY
-
-    static async deleteProperty(req,res, next){
-        try {
-            const propertyToDelete = await PropertyServices.getPropertyById(req.params.id);
-            if (!propertyToDelete){
-                return res.status(409).json({
-                    status: res.statusCode,
-                    message: 'Invalid Input'
-                })
-            }else{
-                const deletedProperty = await PropertyServices.deleteProperty(req.params.id);
-                return res.status(200).json({
-                    status: res.statusCode,
-                    message: 'Property deleted successfully',
-                    data: deletedProperty
-                })
-            }
-        } catch (error) {
-            return next(error);
-        }
+  static async deleteProperty(req, res, next) {
+    try {
+      const propertyToDelete = await PropertyServices.getPropertyById(
+        req.params.id
+      );
+      if (!propertyToDelete) {
+        return res.status(409).json({
+          status: res.statusCode,
+          message: "Invalid Input",
+        });
+      } else {
+        const deletedProperty = await PropertyServices.deleteProperty(
+          req.params.id
+        );
+        return res.status(200).json({
+          status: res.statusCode,
+          message: "Property deleted successfully",
+          data: deletedProperty,
+        });
+      }
+    } catch (error) {
+      return next(error);
     }
+  }
 
-   
-    static async updateProperty(req,res,next){
-        try {
-            const propertyToUpdate = req.body;
-            
-            const getProperty = await PropertyServices.getPropertyById(req.params.id);
-            if (getProperty){
-                
-                const updateProperty = await PropertyServices.updateProperty(req.params.id, propertyToUpdate);
-                if(updateProperty){
-                    return res.status(200).json({
-                        status: res.statusCode,
-                        message: 'property updated',
-                        updateProperty
-                        
-                    })
-                }else {
-                    return res.status(409).json({
-                        status: res.statusCode,
-                        message: 'Invalid Input'
-                    })
-                }
-            }else{
-                return res.status(409).json({
-                    status: res.statusCode,
-                    message: 'Property not found'
-                })
-            }
-        } catch (error) {
-            return next(error);
+  static async updateProperty(req, res, next) {
+    try {
+      const propertyToUpdate = req.body;
+
+      const getProperty = await PropertyServices.getPropertyById(req.params.id);
+      if (getProperty) {
+        const updateProperty = await PropertyServices.updateProperty(
+          req.params.id,
+          propertyToUpdate
+        );
+        if (updateProperty) {
+          return res.status(200).json({
+            status: res.statusCode,
+            message: "property updated",
+            updateProperty,
+          });
+        } else {
+          return res.status(409).json({
+            status: res.statusCode,
+            message: "Invalid Input",
+          });
         }
+      } else {
+        return res.status(409).json({
+          status: res.statusCode,
+          message: "Property not found",
+        });
+      }
+    } catch (error) {
+      return next(error);
     }
+  }
 }
 
 module.exports = PropertyControllers;
