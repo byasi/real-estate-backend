dc67116a3ff50c99eafd70d1fbdd6919dfa57487
byasi <solomonbyasi@gmail.com>
2023-05-17 14:28:41 +0300

    added single property

--- dashboard/api/FetchProperties.js
+++ dashboard/api/FetchProperties.js
@@ -1,16 +1,17 @@
 const tbody = document.getElementById("tbody");
 
-  const response = fetch("http://localhost:5000/api/v1/property")
-  .then((response) =>response.json())
+const response = fetch("http://localhost:5000/api/v1/property")
+  .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    if(data.status === 200){
-        const properties = data.data;
-        tbody.innerHTML =  properties.map(
+    if (data.status === 200) {
+      const properties = data.data;
+      tbody.innerHTML = properties
+        .map(
           (property) =>
             `
             <tr key=${property.id}>
-            <td class="table-plus">${property.name}</td>
+            <td class="table-plus"><a href="/dashboard/singleProperty.html?singleproperty=${property.id}">${property.name}</a></td>
             <td>${property.status}</td>
             <td>${property.type}</td>
             <td>${property.area}</td>
@@ -18,13 +19,9 @@ const tbody = document.getElementById("tbody");
             <td>${property.location}</td>
         </tr>
           `
-        ).join("")
-
+        )
+        .join("");
     } else {
-        tbody.innerHTML = ``;
+      tbody.innerHTML = ``;
     }
-  
-
-  })
-  
-
+  });
